a10c5570c4d91d13545570e78f32ea09e2a8c9ba
donut <kylecasey4@gmail.com>
2018-05-09 14:15:13 -0700

    initial commit

---
+++ source/main.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <switch.h>
+
+int main(int argc, char **argv)
+{
+	gfxInitDefault();
+	consoleInit(NULL);
+	int frames = 0;
+    struct timeval secTime, now;
+    gettimeofday(&secTime, NULL);
+	// Main loop
+	while(appletMainLoop())
+	{
+		//Scan all the inputs. This should be done once for each frame
+		hidScanInput();
+		
+		gettimeofday(&now, NULL);
+    	if (now.tv_usec <= secTime.tv_usec){
+      		frames = frames + 1;
+    	}else{
+			consoleClear();
+      		printf("FPS: %d\n", frames);
+      		gettimeofday(&secTime, NULL);
+      		frames = 0;
+    	}
+
+		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
+		u32 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+		if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
+
+		gfxFlushBuffers();
+		//gfxSwapBuffers();
+		//gfxWaitForVsync();
+	}
+
+	gfxExit();
+	return 0;
+}
+
